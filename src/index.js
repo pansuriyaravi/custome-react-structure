eba66a58c29e4bd28af3ca3b9960af460e29bf5e
Zeref <26054135+zerefdragneel3007@users.noreply.github.com>
2019-01-19 00:35:23 +0530

    Create Web Pack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const temp = <h1>Hello React JS</h1>;
+ReactDOM.render(temp, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      }
+    ]
+  },
+  devtool: "cheap-module-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
+};
